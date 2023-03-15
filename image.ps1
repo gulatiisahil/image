871c18af2cd034f520a7a87ce84e10e9e276d6c3
gulatiisahil <78688221+gulatiisahil@users.noreply.github.com>
2023-03-15 17:20:59 +0530

    Update image.ps1

--- image.ps1
+++ image.ps1
@@ -1,5 +1,5 @@
 $npm_version = '9.5.0'
-$npm_installed = Get-Command npm -ErrorAction SilentlyContinue
+$npm_installed = Get-Command "C:\Program Files\nodejs\npm.cmd" -ErrorAction SilentlyContinue
 
 if ($npm_installed -eq $null) {
     Write-Host "npm is not installed. Installing npm version $npm_version..."
@@ -7,12 +7,11 @@ if ($npm_installed -eq $null) {
     Start-Process msiexec.exe -ArgumentList '/i', 'node.msi', '/quiet', '/norestart' -Wait
     Write-Host "npm version $npm_version has been installed."
 }
-else {npm -v
-
-    $npm_version_installed = npm -v
+else {
+    $npm_version_installed = & "C:\Program Files\nodejs\npm.cmd" -v
     if ($npm_version_installed -ne $npm_version) {
         Write-Host "npm version $npm_version_installed is installed. Updating to npm version $npm_version..."
-        npm install -g npm@$npm_version
+        & "C:\Program Files\nodejs\npm.cmd" install -g npm@$npm_version
         Write-Host "npm has been updated to version $npm_version."
     }
     else {
